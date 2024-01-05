ed2fdb1f031eb01c10d6017cf9a781f5f8f713fc
12001226CTC <12001226CTC@MA-CTC-SDD-S14>
2024-01-05 12:57:32 -0500

    Add project files.

---
+++ SnakeGame/Audio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace SnakeGame
+{
+    public static class Audio
+    {
+        public readonly static MediaPlayer GameOver = LoadAudio("death.wav");
+        private static MediaPlayer LoadAudio(string filename)
+        {
+            MediaPlayer player = new();
+            player.Open(new Uri($"Sounds/{filename}", UriKind.Relative));
+
+                return player;
+        }
+    }
+}
